608348b27dc185ce5bdd1aa811aa7e0b51cf9dfa
ACFcoding <adrianc.franks@gmail.com>
2023-06-22 17:09:05 -0600

    Finalized code

--- server.js
+++ server.js
@@ -12,3 +12,36 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 //re study above lines
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+app.get('/api/notes', (req, res) => {
+fs.readFile('db/db.json', 'utf-8', (err, data) => {
+  //if error, console log the error, else parse the data
+  err ? console.log('Error', err) : res.json(JSON.parse(data))
+})
+});
+
+app.post('/api/notes', (req, res) => {
+let title = req.body.title
+let text = req.body.text
+let createdNote = {
+  title, text, id: uuidv4()
+}
+fs.readFile('db/db.json', 'utf-8', (err, data) => {
+  let savedNotes = JSON.parse(data)
+  savedNotes.push(createdNote)
+  fs.writeFile('db/db.json', JSON.stringify(savedNotes), (err) =>{
+    err ? console.log('Error', err) : console.log('Success,' + createdNote.title + 'has been saved!')
+    console.log("createdNote", createdNote)
+  })
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
